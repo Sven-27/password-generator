2e25bc48bc933357e0077973c3e5565346f7bf44
Sven-27 <github.geometric664@passinbox.com>
2025-10-26 09:19:05 +0100

    styled length component still need to make slider active

--- src/components/generate/length.jsx
+++ src/components/generate/length.jsx
@@ -1,6 +1,19 @@
 function length() {
   return (
-    <div>Character Length</div>
+    <div className="w-full flex flex-col mb-4">
+      <label for="length" className="text-grey-200 flex justify-between items-center mb-4 text-[clamp(16px,4vw,18px)]">
+        Character Length
+        <span id="number" className="mr-2 text-green text-[clamp(24px,4vw,32px)]">0</span>
+      </label>
+      <input
+        type="range"
+        id="length"
+        className="w-full appearance-none  bg-black-200 h-2 slider"
+        name="length"
+        min="0"
+        max="64"
+      />
+    </div>
   )
 }
 
