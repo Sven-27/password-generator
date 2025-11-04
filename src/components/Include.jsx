eaeb0d4b5c4835a12321e3d8d20133f847581ebd
Sven-27 <github.geometric664@passinbox.com>
2025-11-04 20:09:15 +0100

    styled include component with active check

--- src/components/Include.jsx
+++ src/components/Include.jsx
@@ -1,11 +1,23 @@
 function include() {
   return (
-    <div>
-      Include Uppercase Letters
-  Include Lowercase Letters
-  Include Numbers
-  Include Symbols
-    </div>
+    <>
+      <div className="flex items-center  mb-4">
+        <input type="checkbox" id="uppercase" />
+        <label for="uppercase" className="ml-2 text-grey-200 text-[clamp(16px,4vw,18px)]">Include Uppercase Letters</label>
+      </div>
+      <div className="flex items-center mb-4">
+        <input type="checkbox" id="lowercase" />
+        <label for="lowercase" className="ml-2 text-grey-200 text-[clamp(16px,4vw,18px)]">Include Lowercase Letters</label>
+      </div>
+      <div className="flex items-center mb-4">
+        <input type="checkbox" id="numbers" />
+        <label for="numbers" className="ml-2 text-grey-200 text-[clamp(16px,4vw,18px)]">Include Numbers</label>
+      </div>
+      <div className="flex items-center">
+        <input type="checkbox" id="symbols" />
+        <label for="symbols" className="ml-2 text-grey-200 text-[clamp(16px,4vw,18px)]">Include Symbols</label>
+      </div>
+    </>
   )
 }
 
