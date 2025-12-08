3da705bab7abc90763b638d5fcb995e7317eaa33
Sven-27 <github.geometric664@passinbox.com>
2025-12-08 19:56:24 +0100

    added justify-between in strength component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import {useState, useEffect} from "react";
+import {useState} from "react";
 import Result from "./components/Result";
 import Length from "./components/Length";
 import Include from "./components/Include";
--- src/components/Strength.jsx
+++ src/components/Strength.jsx
@@ -2,9 +2,9 @@
 
 function Strength({strength}) {
   return (
-    <div className="bg-grey-900 py-4 px-6 mt-6 flex items-center ">
+    <div className="bg-grey-900 py-4 px-6 mt-6 flex items-center justify-between">
       <p className="text-grey-600 uppercase">Strength</p>
-      <div className="">
+      <div className="flex justify-between ml-4">
         {/* Strength indicator bars */}
         {!strength ? null : strength}
       </div>
