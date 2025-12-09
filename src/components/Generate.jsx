e27a32b37288f2f83c77958d8e18ac69d3abdf1e
Sven-27 <github.geometric664@passinbox.com>
2025-12-09 20:02:22 +0100

    finished styling

--- src/components/Generate.jsx
+++ src/components/Generate.jsx
@@ -1,7 +1,8 @@
 function generate() {
   return (
-    <button>
+    <button className="bg-green w-full py-4 text-grey-800 uppercase font-bold tracking-wider mt-5 hover:opacity-70 transition-opacity flex items-center justify-center gap-2">
       Generate
+      <img src="./src/assets/images/icon-arrow-right.svg" alt="arrow right" className="inline-block ml-2 " />
     </button>
   )
 }
