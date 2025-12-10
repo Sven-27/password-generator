a303faf9825825c15dc649aa909f2cfd25cbdb17
Sven-27 <github.geometric664@passinbox.com>
2025-12-10 19:53:30 +0100

    added hover styling

--- src/components/Generate.jsx
+++ src/components/Generate.jsx
@@ -1,8 +1,8 @@
 function generate() {
   return (
-    <button className="bg-green w-full py-4 text-grey-800 uppercase font-bold tracking-wider mt-5 hover:opacity-70 transition-opacity flex items-center justify-center gap-2">
+    <button className="bg-green w-full py-4 text-grey-800 uppercase font-bold tracking-wider mt-5 hover:bg-transparent hover:text-green border border-green  flex items-center justify-center gap-2">
       Generate
-      <img src="./src/assets/images/icon-arrow-right.svg" alt="arrow right" className="inline-block ml-2 " />
+      <svg width="12" height="12" xmlns="http://www.w3.org/2000/svg"><path fill="#24232C" d="m5.106 12 6-6-6-6-1.265 1.265 3.841 3.84H.001v1.79h7.681l-3.841 3.84z"/></svg> 
     </button>
   )
 }
