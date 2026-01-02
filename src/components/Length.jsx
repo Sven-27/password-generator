5c7cf97eb789cb2d316394703511f1afef05ebdd
Sven-27 <github.geometric664@passinbox.com>
2026-01-02 19:43:47 +0100

    fixed errors

--- src/components/Length.jsx
+++ src/components/Length.jsx
@@ -3,10 +3,10 @@ import RangeSlider from "react-range-slider-input";
 function length({number, setNumber}) {
   return (
     <div className="w-full flex flex-col mb-7">
-      <div id="volume-label" className="text-grey-200 flex justify-between items-center mb-4 text-[clamp(16px,4vw,18px)]">
+      <label id="volume-label" htmlFor="length" className="text-grey-200 flex justify-between items-center mb-4 text-[clamp(16px,4vw,18px)]">
         Character Length
         <span id="number" className="mr-2 text-green text-[clamp(24px,4vw,32px)]">{number === 0 ? number : number[1]}</span>
-      </div>
+      </label>
       <RangeSlider
         id="length"
         className="single-thumb w-full"
@@ -15,7 +15,7 @@ function length({number, setNumber}) {
         max={64}
         step={1}
         value={number}
-        defaultValue={[0,0]}
+        defaultValue={["0","0"]}
         thumbsDisabled={[true, false]}
         onInput={setNumber}
         aria-labelledby="volume-label"
